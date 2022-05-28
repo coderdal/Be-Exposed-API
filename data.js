2334d7c6fcc67a5bc97075833b59c24a7749b56c
coderdal <agircoder@gmail.com>
2022-05-28 18:31:23 +0300

    Files Uploaded

---
+++ data.js
@@ -0,0 +1,67 @@
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = [
+  {
+    id: uuidv4(),
+    path: "/assets/videos/74cdh195by9dzjla2.mp4",
+    subtitle: "Every second of your life you are under attack.",
+    answer: "Hayatınızın her anında saldırı altındasınız.",
+    distractor: "Hayatınızın her anında baskı altındasınız.",
+  },
+  {
+    id: uuidv4(),
+    path: "/assets/videos/5e8t15hj02w33.mp4",
+    subtitle: "The future is now.",
+    answer: "Gelecek, şu andır.",
+    distractor: "Gelecek, bir gizemdir.",
+  },
+  {
+    id: uuidv4(),
+    path: "/assets/videos/vou94sy9udc3yv1.mp4",
+    subtitle: "I will not ask again!",
+    answer: "Tekrar sormayacağım!",
+    distractor: "Tekrar yardım etmeyeceğim!",
+  },
+  {
+    id: uuidv4(),
+    path: "/assets/videos/hu1329i7appjvm3a2.mp4",
+    subtitle: "I believe I can fly.",
+    answer: "Uçabileceğime inanıyorum.",
+    distractor: "İnandım ve uçuyorum.",
+  },
+  {
+    id: uuidv4(),
+    path: "/assets/videos/rvb1hn57k62yi84a2.mp4",
+    subtitle: "90% of trade goes by sea.",
+    answer: "Ticaretin % 90'ı deniz yoluyla yapılıyor.",
+    distractor: "Ulaşımın % 90'ı deniz yoluyla yapılıyor.",
+  },
+  {
+    id: uuidv4(),
+    path: "/assets/videos/7s3sgbp2jnm9d7g.mp4",
+    subtitle: "Music is the language of emotions. ",
+    answer: "Müzik, duyguların dilidir.",
+    distractor: "Müzik, duygusal bir dildir.",
+  },
+  {
+    id: uuidv4(),
+    path: "/assets/videos/7s3sgbp2jnm9d7g.mp4",
+    subtitle: "Music is the language of emotions. ",
+    answer: "Müzik, duyguların dilidir.",
+    distractor: "Müzik, duygusal bir dildir.",
+  },
+  {
+    id: uuidv4(),
+    path: "/assets/videos/d4s8sacdvdedz3ha2.mp4",
+    subtitle: "Every man dies. Not every man really lives. ",
+    answer: "Her insan ölür. Ancak her insan gerçekten yaşamaz.",
+    distractor: "Her insan ölür. Ancak her insan onurluca yaşamaz.",
+  },
+  {
+    id: uuidv4(),
+    path: "/assets/videos/eoaog4yfgdlkkpoa2.mp4",
+    subtitle: "You are not alone. You never were. You never will be.",
+    answer: "Yalnız değilsin. Hiç yalnız olmadın. Hiç yalnız olmayacaksın.",
+    distractor: "Yalnız değildin. Hiç yalnız olmadın. Hiç yalnız olmayacaksın.",
+  },
+];
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const server = express();
+
+const cors = require("cors");
+
+const data = require("./data.js");
+
+server.use(
+  cors({
+    origin: "https://be-exposed.netlify.app",
+  })
+);
+
+/* EndPoints */
+
+server.get("/", (req, res) => {
+  res
+    .status(200)
+    .send(
+      "<h1>This api created for <a href='https://be-exposed.netlify.app'>https://be-exposed.netlify.app</a></h1>"
+    );
+});
+
+server.get("/videos", (req, res) => {
+  res.status(200).json(data);
+});
+
+server.get("/video", (req, res) => {
+  res.status(200).json(data[Math.floor(Math.random() * data.length)]);
+});
+
+server.listen(process.env.PORT || 3001, () => {
+  console.log("Server is running");
+});
